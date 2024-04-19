c7c7e8c8af80f53fce0f1d2fffb1e2dfab463166
AaronAlonsoAparicio <aaron.alonsoaparicio@educa.madrid.org>
2024-04-19 09:31:41 +0200

    Añadidos comentarios en JavaDoc y Refactorizacion

--- Tribial_Challenge/src/clases/Jugador.java
+++ Tribial_Challenge/src/clases/Jugador.java
@@ -2,29 +2,42 @@ package clases;
 
 import java.util.Objects;
 
+/**
+ * Clase que nos define las funcionalidades y caracteristicas de los jugadores.
+ * 
+ * @author Aaron Alonso Aparicio
+ * @version 1.0
+ */
+
 public class Jugador {
-	
+
 	private String nombre;
-	
-	
-	
+
+	/**
+	 * Contructor de los objetos de Jugador.
+	 * @param nombreJugador
+	 */
 	public Jugador(String nombreJugador) {
 		super();
 		this.nombre = nombreJugador;
+
 	}
-
-
+	/**
+	 * Clase que nos dice el nombre de los jugadores.
+	 * @return nombre del Jugador
+	 */
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
-
-
-
-
-
+	/**
+	 * Metodo que nos comprueba mediente el nombre del jugador si ya hay uno igual.
+	 * 
+	 * @param obj Le pasamos el objeto que queremos comprobar
+	 * @return true si el objeto si existe, false sino.
+	 * @since 1.0
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -37,28 +50,36 @@ public class Jugador {
 		return Objects.equals(nombre, other.nombre);
 	}
 
+	/**
+	 * Metodo para imprimir la informacion de los jugadores.
+	 */
 
- public void imprimirInformacion() {
-	 
-	 System.out.println(toString());
-	 
- }
-	
-	
+	public void imprimirInformacion() {
+
+		System.out.println(toString());
+
+	}
 
 	@Override
 	public String toString() {
-		return "Jugador: [ "+ nombre + "]";
+		return "Jugador: [" + nombre + "]";
 	}
 
-	
-	
+	/**
+	 * Metodo que no comprueba que el nombre del jugador esta correctamente formado.
+	 */
+	public void comprobarNombreJugador() {
+		int contador = 0;
+		while (contador < this.nombre.length()) {
+			if (!Character.isLetter(this.nombre.charAt(contador))) {
+				System.out.println("Nombre invadilo.");
+				System.out.println("El nombre de los jugadores humanos no pueden contener numeros.");
 
-	
+			}
+			System.out.println("Nombre valido.");
 
-	
-	
-	
-	
+		}
+
+	}
 
 }
--- Tribial_Challenge/src/clases/Tribial_Challenge.java
+++ Tribial_Challenge/src/clases/Tribial_Challenge.java
@@ -1,4 +1,4 @@
-package clases; 
+package clases;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -6,139 +6,179 @@ import constantes.constantesPartida;
 
 /**
  * Practica tercer trimestre, programa principal.
+ * 
  * @author Aaron Alonso Aparicio
- * @version 1.0 
+ * @version 1.0
  * 
  */
 public class Tribial_Challenge {
 	static Scanner teclado;
-	
+	static ArrayList<Jugador> jugadores;
+
 	public static void main(String[] args) {
-		
+
 		presentacionPrograma();
 		opcionesPrograma();
 		menuJugadores();
-		
-		
-		
+
 	}
-	
-	
+
 	/**
 	 * Menu de las opciones propias que tienen los jugadores
+	 * 
 	 * @since 1.0
 	 */
 	private static void menuJugadores() {
-		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();
+		jugadores = new ArrayList<Jugador>();
 		int opcion;
-		do {
-			
-		
+
 		teclado = new Scanner(System.in);
 		System.out.println("Has seleccionado la opcion: Jugadores");
 		System.out.println("Que deseas hacer ahora:");
 		System.out.println("Antes de hacer nada dinos tu nombre para poder identificarte");
 		String nombre = teclado.next();
-		Jugador nuevoJugador = new Jugador(nombre);
-		System.out.println("1) Ver jugadores");
-		System.out.println("2) Añadir jugador");
-		System.out.println("3) Eliminar jugador");
-		System.out.println("4) Volver al menu principal");
-		opcion = teclado.nextInt();
-		switch (opcion) {
-		case 1:
-			if(jugadores.isEmpty()) {
-				System.out.println("Todavia no hay nigun jugador registrador.");
-				System.out.println("Porfavor seleciona la opcion 2 para registarse");
+		do {
+
+			Jugador nuevoJugador = new Jugador(nombre);
+			System.out.println("1) Ver jugadores");
+			System.out.println("2) Añadir jugador");
+			System.out.println("3) Eliminar jugador");
+			System.out.println("4) Volver al menu principal");
+			opcion = teclado.nextInt();
+			switch (opcion) {
+			case 1:
+				casoUnoJugadores();
+
+				break;
+			case 2:
+
+				casoDosJugadores(nuevoJugador);
+
+				break;
+
+			case 3:
+				
+				
+				
+				
+				
+				
+				
+
+				break;
+
+			case 4:
+
+				opcionesPrograma();
+				break;
+
+			default:
+				break;
 			}
-			else {
-				
-				for (Jugador jugador : jugadores) {
-					jugador.imprimirInformacion();
-				}
-				
-			}
-			
-			break;
-		case 2:
-			if(!jugadores.contains(nuevoJugador)) {
-				System.out.println("Registrado correctamente.");
-				jugadores.add(nuevoJugador);
-				
-				
-				
-			}else {
-				System.out.println("Ha habido algun problema con su identificacion.");
-			}
-			
-			
-			
-			break;
-
-		case 3:
-	
-			break;
-
-		case 4:
-	
-			opcionesPrograma();
-			break;
-
-
-		default:
-			break;
-		}
 		} while (opcion != 5);
 	}
-
-
-
+	
 	
 	
 	/**
+	 * Metodo del caso 1 del menu de los jugadores.
+	 * 
+	 */
+
+	private static void casoUnoJugadores() {
+		if (jugadores.isEmpty()) {
+			System.out.println("Todavia no hay nigun jugador registrador.");
+			System.out.println("Porfavor seleciona la opcion 2 para registarse");
+		} else {
+
+			for (Jugador jugador : jugadores) {
+				jugador.imprimirInformacion();
+			}
+
+		}
+	}
+
+	/**
+	 * Metodo del caso dos del menu de los jugadores
+	 * @param nuevoJugador
+	 * @since 1.0
+	 * 
+	 */
+	
+	private static void casoDosJugadores(Jugador nuevoJugador) {
+		if (!jugadores.contains(nuevoJugador)) {
+			System.out.println("Registrado correctamente.");
+			jugadores.add(nuevoJugador);
+
+		} else {
+			System.out.println("Ha habido algun problema con su identificacion.");
+		}
+
+		System.out.println("¿Quieres añadir un nuevo jugador?");
+		System.out.println("(Si/No)");
+		String registrar = teclado.next();
+
+		if (registrar.equalsIgnoreCase("Si")) {
+			System.out.println("¿Dime el nombre del nuevo jugador?");
+			String nombreJugador = teclado.next();
+			Jugador anaydirNuevoJugador = new Jugador(nombreJugador);
+			if (!jugadores.contains(anaydirNuevoJugador)) {
+				System.out.println("Añadido correctamente el jugador: " + nombreJugador);
+				jugadores.add(anaydirNuevoJugador);
+
+			} else {
+				System.out.println("Jugador ya existente en el sistema.");
+				System.out.println("Tendra que eliminar el jugador " + nombreJugador + " para poder registrarse.");
+
+			}
+
+		}
+	}
+
+	/**
 	 * Creamo el menu de opciones principal que vamos a usar en el programa.
+	 * 
 	 * @since 1.0
 	 */
 	private static void opcionesPrograma() {
 		teclado = new Scanner(System.in);
-		System.out.println("Vamos a ver que deciden hacer los jugadores de hoy:");
+		System.out.println("¿Qué quieres hacer?");
 		System.out.println("Podemos elegir entre:");
-		System.out.println("Jugar Partida.");
-		System.out.println("Ranking.");
-		System.out.println("Historial.");
-		System.out.println("Jugadores.");
-		System.out.println("Salir.");
+		System.out.println("1) Jugar Partida.");
+		System.out.println("2) Ranking.");
+		System.out.println("3) Historial.");
+		System.out.println("4) Jugadores.");
+		System.out.println("5) Salir.");
 
 		System.out.println("¿Que deseas hacer?");
-		String opcionElegida = teclado.next();
+		int opcionElegida = teclado.nextInt();
 		switch (opcionElegida) {
-		case "Jugar Partida":
-			
+		case 1:
+
 			break;
-			
-		case "Ranking":
-			
+
+		case 2:
+
 			break;
-		case "Historial":
-			
+		case 3:
+
 			break;
-			
-		case "Jugadores" :
-			
+
+		case 4:
+
 			break;
-			
-		case "Salir":
+
+		case 5:
 		default:
 			System.out.println("Hasta la proxima.");
 			break;
 		}
 	}
-	
-	
-	
+
 	/**
 	 * Presentacion del programa.
-	 * @since 1.0  
+	 * 
+	 * @since 1.0
 	 */
 
 	private static void presentacionPrograma() {
@@ -146,13 +186,14 @@ public class Tribial_Challenge {
 		System.out.println("     Acierta y gana    ");
 		System.out.println("Un programa prensentado por Aaron Alonso.");
 		System.out.println("¿Cuantos jugadores humanos van a participar?");
-		teclado = new Scanner(System.in); 
+		teclado = new Scanner(System.in);
 		int jugadoresHumanos = teclado.nextInt();
-		System.out.println("En el dia a de hoy van a participar  " + jugadoresHumanos +" jugadores" );
+		System.out.println("En el dia a de hoy van a participar " + jugadoresHumanos + " jugador/es");
 		int jugadoresMaquina = constantesPartida.MAX_JUGADORES - jugadoresHumanos;
 		System.out.println("Y con " + jugadoresMaquina + " jugadores maquina");
 		int numJugadores = jugadoresHumanos + jugadoresMaquina;
-		System.out.println("Hoy van a particiar " + numJugadores + " demoles la bienvenida."  );
+		System.out.println("Hoy van a particiar " + numJugadores + "jugadores demoles la bienvenida.");
+		
 	}
 
 }
