8c698f389b9f912b92372145e624c2988c359cd5
AaronAlonsoAparicio <aaron.alonsoaparicio@educa.madrid.org>
2024-04-22 10:00:25 +0200

    Cambios en la clase pregntas

--- Tribial_Challenge/src/clases/Pregunta.java
+++ Tribial_Challenge/src/clases/Pregunta.java
@@ -1,5 +1,6 @@
 package clases;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -11,50 +12,41 @@ import constantes.ConstantesPreguntas;
  */
 public class Pregunta {
 	private int numeroPregunta;
-	
-	
+
 	public int getTipoPregunta() {
 		return numeroPregunta;
 	}
-	
-	
+
 	/**
 	 * Metodo que nos genera un numero aleatorio del 1 al 3
+	 * 
 	 * @return tipoDePreguntaGenerada
 	 */
 	public static int preguntas() {
-		Random preguntaAleatoria = new Random(); 
-		int tipoDePreguntaGenerada = preguntaAleatoria.nextInt(3) +1 ;
+		Random preguntaAleatoria = new Random();
+		int tipoDePreguntaGenerada = preguntaAleatoria.nextInt(3) + 1;
 		return tipoDePreguntaGenerada;
 	}
-	
-	
-	
+
 	/**
 	 * Metodo que nos va a generar las preguntas del programa.
-	 * @param tipoPregunta	unos da un numero conrrespondinte a cada tipo de pregunta.
+	 * 
+	 * @param tipoPregunta unos da un numero conrrespondinte a cada tipo de
+	 *                     pregunta.
 	 * 
 	 */
 	public static void generarPreguntas(int tipoPregunta) {
 		tipoPregunta = preguntas();
-		if(tipoPregunta == ConstantesPreguntas.PREGUNTA_MATES) {
-			
-			
-		}else if(tipoPregunta == ConstantesPreguntas.PREGUNTA_LENGUA){
-			
-		}else {
-			
+		if (tipoPregunta == ConstantesPreguntas.PREGUNTA_MATES) {
+
+		} else if (tipoPregunta == ConstantesPreguntas.PREGUNTA_LENGUA) {
+
+		} else {
+
 			Path rutaArchivoInlges = Paths.get("./../../materiales/ingles.txt");
-			
-			
-			
-			
+
 		}
-		
+
 	}
 
-	
-
 }
-
-
