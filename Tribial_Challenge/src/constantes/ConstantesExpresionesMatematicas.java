90d676ad3523b33d8980cbaaeaf9cb211b11b51d
AaronAlonsoAparicio <aaron.alonsoaparicio@educa.madrid.org>
2024-05-03 22:08:03 +0200

    Preguntas casi, añadida libreria, nuevas constantes

---
+++ Tribial_Challenge/src/constantes/ConstantesExpresionesMatematicas.java
@@ -0,0 +1,13 @@
+package constantes;
+
+/**
+ * Clase que nos define las constantes de las expresiones matematicas que vamos a usar en el programa
+ * @author Aaron Alonso
+ */
+public class ConstantesExpresionesMatematicas {
+	
+	public static final String EXPRESION_MULTIPLICACION = "*";
+	public static final String EXPRESION_RESTA = "-";
+	public static final String EXPRESION_SUMA = "+";
+
+}
